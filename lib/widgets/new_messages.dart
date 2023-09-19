6b4f92f409d4b4851bd1f08f27f7e9e1cc0270d5
Vedant Agrawal <96226581+vedantagrawal524@users.noreply.github.com>
2023-09-19 23:27:32 +0530

    Chat Screens Installed

---
+++ lib/widgets/new_messages.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class NewMessages extends StatefulWidget {
+  const NewMessages({super.key});
+
+  @override
+  State<NewMessages> createState() {
+    return _NewMessagesState();
+  }
+}
+
+class _NewMessagesState extends State<NewMessages> {
+  final _messageController = TextEditingController();
+
+  @override
+  void dispose() {
+    _messageController.dispose();
+    super.dispose();
+  }
+
+  void _submitMessage() {
+    final enteredMessage = _messageController.text;
+    if (enteredMessage.trim().isEmpty) {
+      return;
+    }
+
+    _messageController.clear();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 15, right: 1, bottom: 14),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              textCapitalization: TextCapitalization.sentences,
+              autocorrect: true,
+              enableSuggestions: true,
+              decoration: const InputDecoration(labelText: 'Send a message...'),
+              controller: _messageController,
+            ),
+          ),
+          IconButton(
+            onPressed: _submitMessage,
+            icon: const Icon(Icons.send),
+          )
+        ],
+      ),
+    );
+  }
+}
